5c47fb5dd369e48494dde103fea798ebdc236621
MinilikBelachew <belachewr35@gmail.com>
2024-05-23 23:17:23 +0300

    first commit

--- src/components/Maps/MapOne.tsx
+++ src/components/Maps/MapOne.tsx
@@ -3,8 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Import Leaflet CSS
 
 const MapOne: React.FC = () => {
-  const mapPosition = [8.4746, 38.7604]; // Center on Ethiopia
-  const zoomLevel = 6;
+  const mapPosition: [number, number] = [8.4746, 38.7604]; // Center on Ethiopia
+  const zoomLevel: number = 6;
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-7">
@@ -23,7 +23,7 @@ const MapOne: React.FC = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {/* Add Marker for a specific location */}
-          <Marker position={[8.4746, 38.7604]}>
+          <Marker position={mapPosition}>
             {/* Add Popup for the marker */}
             <Popup>
               Add any additional information about this location here.
